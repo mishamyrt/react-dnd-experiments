a80d6b6f368f08adf5ee061c120fa28a3f6bc055
mishamyrt <misha@myrt.co>
2023-10-18 11:56:14 +0300

    feat: randomize mock cards count

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,24 +1,27 @@
+import { randomInt } from '$utils'
 import { CardDataGenerator } from './generate'
 import { type KanbanBoardData } from './types'
 
 const generator = new CardDataGenerator()
 
+const randomLength = (): number => randomInt(30, 5)
+
 export const boardData: KanbanBoardData = [
   {
     title: 'Новый',
-    items: generator.getArray(30),
+    items: generator.getArray(randomLength()),
   },
   {
     title: 'Потенциальный',
-    items: generator.getArray(30),
+    items: generator.getArray(randomLength()),
   },
   {
     title: 'Действующий',
-    items: generator.getArray(30),
+    items: generator.getArray(randomLength()),
   },
   {
     title: 'Расторгнут',
-    items: generator.getArray(30),
+    items: generator.getArray(randomLength()),
   },
 ]
 
